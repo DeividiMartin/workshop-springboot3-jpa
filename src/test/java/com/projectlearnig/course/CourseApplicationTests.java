3597dc157ac0bdb3de7cb1025152fd9613c92c08
DeividiMartin <deividi_97@hotmail.com>
2023-11-04 23:51:57 -0300

    project created

---
+++ src/test/java/com/projectlearnig/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectlearnig.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
