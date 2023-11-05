fdc321c982bc5c55aab45b620c51027c46f29914
DeividiMartin <deividi_97@hotmail.com>
2023-11-05 16:51:10 -0300

    User entity adn resource

---
+++ src/main/java/com/projectlearnig/course/entities/User.java
@@ -0,0 +1,78 @@
+package com.projectlearnig.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String passwoerd;
+    public User(){
+    }
+
+    public User(Long id, String name, String email, String phone, String passwoerd) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.passwoerd = passwoerd;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPasswoerd() {
+        return passwoerd;
+    }
+
+    public void setPasswoerd(String passwoerd) {
+        this.passwoerd = passwoerd;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
