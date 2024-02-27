93b84d8d2da5bb7dbb2d3d77ec2e776cfee9a593
DeividiMartin <deividi_97@hotmail.com>
2024-02-26 21:26:21 -0300

    h2 database, test profile, JPA

--- course/src/main/java/com/projetospring/course/resources/UserResource.java
+++ course/src/main/java/com/projetospring/course/resources/UserResource.java
@@ -14,6 +14,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
+		
 		User u = new User(1L, "Maria","maria@gmail.com", "54999876565", "123321");
 		return ResponseEntity.ok().body(u);
 	}
