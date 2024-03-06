ed2d81e916b7fb0b87a8931d0238ca79eb8c1555
DeividiMartin <deividi_97@hotmail.com>
2024-03-05 22:37:03 -0300

    Payment, one-to-one association

--- course/src/main/java/com/projetospring/course/config/Testconfig.java
+++ course/src/main/java/com/projetospring/course/config/Testconfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetospring.course.entities.Category;
 import com.projetospring.course.entities.Order;
 import com.projetospring.course.entities.OrderItem;
+import com.projetospring.course.entities.Payment;
 import com.projetospring.course.entities.Product;
 import com.projetospring.course.entities.User;
 import com.projetospring.course.entities.enums.OrderStatus;
@@ -81,7 +82,10 @@ public class Testconfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 		
+		orderRepository.save(o1);
 	}
 
 }
