53e75bfca9cdc6d4c3436831019313bfdff37bc2
DeividiMartin <deividi_97@hotmail.com>
2024-03-07 10:31:00 -0300

    User update

--- course/src/main/java/com/projetospring/course/services/UserService.java
+++ course/src/main/java/com/projetospring/course/services/UserService.java
@@ -39,14 +39,20 @@ public class UserService {
 	 }
 	 
 	 public User update (Long id, User obj) {
-		 User entity = repository.getReferenceById(id);
-		 updateData(entity,obj);
-		 return repository.save(entity);
+			try {
+				User entity = repository.getReferenceById(id);
+				updateData(entity,obj);
+				return repository.save(entity);
+			}catch (RuntimeException e) {
+				throw new ResourceNotFoundException(id);
+			}
 		 
 	 }
 	private void updateData(User entity, User obj) {
-		entity.setName(obj.getName());
-		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());
-	}
+	
+			entity.setName(obj.getName());
+			entity.setEmail(obj.getEmail());
+			entity.setPhone(obj.getPhone());
+		
+		}
 }
